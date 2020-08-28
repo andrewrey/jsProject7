df2c28584a3bfeaa411db1b6268cdbf30e226036
Andrew Reynolds <andrew_reynolds@mac.com>
2020-08-28 12:32:15 -0700

    completion of game play functionality

--- js/app.js
+++ js/app.js
@@ -1,44 +1,74 @@
 // DOM Elements
 const wordEl = document.getElementById("word");
 const wrongLettersEl = document.getElementById("wrong-letters");
-const playAgainBtn = document.getElementById("play-again");
+const playAgainBtn = document.getElementById("play-button");
 const popup = document.getElementById("popup-container");
 const notification = document.getElementById("notification-container");
 const finalMessage = document.getElementById("final-message");
 
-const figureParts = document.querySelectorAll("figure-part");
+const figureParts = document.querySelectorAll(".figure-part");
 
 const words = ["application", "programming", "interface", "wizard"];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = ["p", "r", "o", "g", "r", "a", "m", "m", "i", "n", "g"];
+const correctLetters = [];
 const wrongLetters = [];
+
+function convertToSpan(word) {
+  let splitWord = word.split("");
+  let arrayOfSpans = splitWord.map((letter) => {
+    return `
+      <span class="letter">
+        ${correctLetters.includes(letter) ? letter : ""}
+      </span>
+    `;
+  });
+  let joinedSpans = arrayOfSpans.join("");
+  return joinedSpans;
+}
 
 // Show the hidden word
 function displayWord() {
-  wordEl.innerHTML = `
-    ${selectedWord
-      .split("")
-      .map(
-        (letter) => `
-          <span class="letter">
-            ${correctLetters.includes(letter) ? letter : ""}
-          </span>
-        `
-      )
-      .join("")}
-  `;
+  wordEl.innerHTML = `${convertToSpan(selectedWord)}`;
   const innerWord = wordEl.innerText.replace(/\n/g, "");
-
   if (innerWord === selectedWord) {
     finalMessage.innerText = "Congratulations! you won!";
     popup.style.display = "flex";
   }
 }
 
+function displayWrongLetters(wrongL) {
+  let wrongLetterArray = wrongL.map(
+    (letter) => `
+  <span>${letter}</span>
+ `
+  );
+  console.log(wrongLetterArray);
+  return wrongLetterArray;
+}
+
 // Update the wrong letters
 function updateWrongLettersEl() {
-  console.log("Update Wrong");
+  // Display Wrong Letters
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? `<p>Wrong</p>` : ""}
+    ${displayWrongLetters(wrongLetters)}
+  `;
+
+  // Display parts of the body base on number of errors
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+    if (index < errors) {
+      part.style.display = "block";
+    } else {
+      part.style.display = "none";
+    }
+  });
+  // Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = `Unfortunately you lost. 🥺`;
+    popup.style.display = "flex";
+  }
 }
 
 // Show notification
@@ -68,6 +98,17 @@ window.addEventListener("keydown", (e) => {
       }
     }
   }
+});
+
+// Restart Game and play Again
+playAgainBtn.addEventListener("click", () => {
+  // empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+  popup.style.display = "none";
+  updateWrongLettersEl();
+  displayWord();
 });
 
 displayWord();
